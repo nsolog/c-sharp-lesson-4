623cffbe0d09ea16b23e6bcfa404dfef5c48a084
Nikita Smetannikov <smetannikovnikita@yandex.ru>
2022-08-30 15:27:30 +0300

    Task 1 resolved

---
+++ task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+// Зачем тут в условии цикл?
+
+for (int i = 0; i < 100; i++)
+{
+    Console.WriteLine("Введите 2 числа");
+    int numberA = Convert.ToInt32(Console.ReadLine());
+    int numberB = Convert.ToInt32(Console.ReadLine());
+
+    double result = Math.Pow(numberA, numberB);
+    
+    Console.WriteLine($"{numberA} в степени {numberB} = {result}");
+    Console.WriteLine();
+}
