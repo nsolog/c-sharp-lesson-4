6acb2066504c363ca7fddb878157b9f4c946a324
Nikita Smetannikov <smetannikovnikita@yandex.ru>
2022-08-30 16:08:33 +0300

    Task 2 resolved

---
+++ task2/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+Console.Write("Введите число: ");
+string number = Console.ReadLine();
+
+int result = 0;
+for (int i = 0; i < number.Length; i++)
+{
+    result += int.Parse(number[i].ToString());
+}
+
+Console.WriteLine($"Сумма цифр в числе = {result}");
