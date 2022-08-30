1066599d11c918629dda7c0d4dd859fa76064de5
Nikita Smetannikov <smetannikovnikita@yandex.ru>
2022-08-30 17:10:40 +0300

    Task 3 resolved

---
+++ task3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+Console.WriteLine("Введите 8 элементов: ");
+int[] arr = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    Console.Write($"{i+1}: ");
+    int temp = Convert.ToInt32(Console.ReadLine());
+    arr[i] = temp;
+}
+
+void showArray(int[] array)
+{
+    Console.WriteLine();
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+}
+
+showArray(arr);
